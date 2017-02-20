d110a31987caba0e98dc606eb4585a8ff10d622a
AdityaShaha <aditya.shaha.p@gmail.com>
2017-02-20 14:14:25 +0530

    Fixed CMY

--- RGBtoCMY.m
+++ RGBtoCMY.m
@@ -1,20 +1,24 @@
-img = imread('Image01.png');
+img = imread('Image03.png');
 red = im2double(img(:,:,1)); % Red channel
 green = im2double(img(:,:,2)); % Green channel
 blue = im2double(img(:,:,3)); % Blue channel
 [Y, X, Z] = size(img);
 % By the method of 
-for i = 1:Y
-    for j = 1:X
-        var_C(i,j) = 1- (red(i,j)/255);
-        var_M(i,j) = 1- (green(i,j)/255);
-        var_Y(i,j) = 1 -(blue(i,j)/255);
+%for i = 1:Y
+    %for j = 1:X
+        %var_C(i,j) = 1- (red(i,j)/255);
+        %var_M(i,j) = 1- (green(i,j)/255);
+        %var_Y(i,j) = 1 -(blue(i,j)/255);
         
-    end
-end
+    %end
+%end
 
+var_C = 1 - red;
+var_M = 1 - green;
+var_Y = 1 - blue;
 CMYImage = cat(3,var_C,var_M,var_Y);
 
 %imshow(img);
+imwrite(CMYImage,'CMYImage-3.png');
 imshow(CMYImage);
  
